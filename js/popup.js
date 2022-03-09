db2ef0475baaae72b60481802d9195e340be3978
alexander1970 <alexander_shloma@mail.ru>
2022-03-09 15:00:25 +0300

    miniature

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,8 @@
+import { createKekstgram } from "./data.js";
+
 const userDialog = document.querySelector('.big-picture');
 userDialog.classList.remove('hidden');
+
+const miniature = document.querySelector('#picture');
+
+const newKekstgram = createKekstgram();
