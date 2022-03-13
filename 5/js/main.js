6698455d13944f142dd38204c67229669353911c
Keks <keks@htmlacademy.ru>
2022-03-13 19:31:40 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,4 @@
+import './util.js';
+import './data.js';
+import './picture.js';
+// import './window.js';
---
+++ 5/js/window.js
@@ -0,0 +1,12 @@
+import { createKekstgram } from "./data.js";
+
+const userWindow = document.querySelector('.big-picture');
+userWindow.classList.remove('hidden');
+
+const arrayWindow = createKekstgram();
+
+arrayWindow.forEach((keks) => {
+  const windowElement = userWindow.cloneNode(true);
+  // windowElement.querySelector('.big-picture__img')
+  userWindow.appendChild(windowElement);
+});
