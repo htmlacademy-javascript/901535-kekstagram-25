9f83d77e76f9aac1b7d491c0c8b5ab1bd4f05519
alexander1970 <alexander_shloma@mail.ru>
2022-03-22 23:27:23 +0300

    win-6

--- js/window.js
+++ js/window.js
@@ -1,23 +1,53 @@
-import { arrayMiniature } from "./picture.js";
 import { isEscapeKey } from "./util.js";
 
 const body = document.querySelector('body');
-const userWindow = document.querySelector('.big-picture');
-const userWindowClose = userWindow.querySelector('.cancel');
+const bigPicture = document.querySelector('.big-picture');
+const social = bigPicture.querySelector('.social');
+const socialCommentCount = social.querySelector('.social__comment-count');
+const bigPictureCancel = bigPicture.querySelector('.cancel');
+const overlay = document.querySelector('.ovelay')
 
-const openWin = () => {
-  userWindow.classList.remove('hidden');
-  body.classList.add('modal-open');
-
-  // userWindow.querySelector('.big-picture__img img').src = arrayMiniature.url;
-
-  document.addEventListener('keydown', (evt) => {
-    if (isEscapeKey(evt)) {
-      evt.preventDefault();
-      userWindow.classList.add('hidden');
-      body.classList.remove('modal-open');
-    }
-  });
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeBigPicture();
+  }
 };
 
-export {openWin};
+const onOverlayClick = (evt) => {
+  if (!evt.target.closest('.big-picture__preview')) {
+    closeBigPicture();
+  }
+};
+
+const openWin = (post) => {
+  bigPicture.querySelector('.big-picture__img img').src = post.url;
+  socialCommentCount.querySelector('.comments-count').textContent = post.comments.length;
+  social.querySelector('.likes-count').textContent = post.likes;
+  social.querySelector('.social__caption').textContent = post.description;
+
+  socialComments.innerHTML = '';
+  socialComments.appendChild(createCommentsFragment(post.comments));
+};
+
+const closeBigPicture = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+  bigPictureCancel.removeEventListener('click', closeBigPicture);
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  overlay.removeEventListener('click', onOverlayClick);
+}
+
+const postOpen = (post) => {
+  openWin(post);
+
+  bigPicture.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  bigPictureCancel.removeEventListener('click', closeBigPicture);
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  overlay.removeEventListener('click', onOverlayClick);
+}
+
+export {postOpen};
